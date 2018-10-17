966e815eb6d0cec2c677b378b39ab89d90736be0
Mahabir Singh <mahabir.singh@daimler.com>
2018-10-17 12:16:50 +0530

    Added eureka related configuration

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Spring boot config server which provides configuration file to it's client based on profiles from configured config-repository...
 add set -Dhttps.proxyHost= -Dhttps.proxyPort= as VM Argument if it is required ?  when you have used cloud url like 
 uri: https://github.com/singhmahabir/config-repository
+
+Runs on port 8701
--- src/main/java/singh/mahabir/config/ConfigServerApplication.java
+++ src/main/java/singh/mahabir/config/ConfigServerApplication.java
@@ -3,17 +3,25 @@ package singh.mahabir.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ *
+ * @author Mahabir Singh
+ *
+ */
 
 @SpringBootApplication
+
 /*
  * To start config server you have to use @EnableConfigServe
  */
 @EnableConfigServer
 
 /*
- * this annotation is required when Config-client will be used through registry server.
+ * this annotation is required when Config-client will consumed properties files through registry server.
  */
-// @EnableEurekaClient
+@EnableEurekaClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
